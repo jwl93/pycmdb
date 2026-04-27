0335bc34a640592734db420afa195d3719984844
jwl <408066557@qq.com>
2026-04-27 11:16:35 +0800

    up

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,1 +1,8 @@
-# CMDB scripts
+"""pycmdb - Git-based CMDB"""
+import os
+from pathlib import Path
+
+
+def get_cmdb_root() -> Path:
+    """Get CMDB root directory from CMDB_ROOT env var, default current directory."""
+    return Path(os.environ.get("CMDB_ROOT", "."))
